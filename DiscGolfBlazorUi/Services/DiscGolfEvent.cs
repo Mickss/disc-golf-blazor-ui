a36117dbea3c14ced84b0c615c459b41cb812f9e
Michał Guz <michal.guz.85@gmail.com>
2025-06-25 10:09:57 +0200

    Replace project with new project

---
+++ DiscGolfBlazorUi/Services/DiscGolfEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace DiscGolfBlazorUi.Services
+{
+    public class DiscGolfEvent
+    {
+        public string? Id { get; set; }
+        public string? Pdga { get; set; }
+        public string? TournamentTitle { get; set; }
+        public string? Region { get; set; }
+        public string? Registration { get; set; }
+        public string? Vacancies { get; set; }
+    }
+}
