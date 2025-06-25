950cdf678f4b33689b50ec53c065e9b037bac138
Michał Guz <michal.guz.85@gmail.com>
2025-06-25 15:00:47 +0200

    Add custom CSS with table

--- DiscGolfBlazorUi/Program.cs
+++ DiscGolfBlazorUi/Program.cs
@@ -1,23 +1,23 @@
-using DiscGolfBlazorU.Components;
+using DiscGolfBlazorUi.Components;
+using DiscGolfBlazorUi.Components.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddScoped(sp => new HttpClient());
+builder.Services.AddScoped<DiscGolfService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
