a8a475b1b6e2216207528f9fe6babf190ca57bad
Michał Guz <michal.guz.85@gmail.com>
2025-06-25 13:33:34 +0200

    Initial working version of DiscGolf app

---
+++ DiscGolfBlazorUi/Components/Services/DiscGolfEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace DiscGolfBlazorUi.Components.Services
+{
+    public class DiscGolfEvent
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Pdga { get; set; } = string.Empty;
+        public string TournamentTitle { get; set; } = string.Empty;
+        public string Region { get; set; } = string.Empty;
+        public string Registration { get; set; } = string.Empty;
+        public string Vacancies { get; set; } = string.Empty;
+    }
+}
---
+++ DiscGolfBlazorUi/Components/Services/DiscGolfService.cs
@@ -0,0 +1,18 @@
+﻿namespace DiscGolfBlazorUi.Components.Services
+{
+    public class DiscGolfService
+    {
+        private readonly HttpClient httpClient;
+
+        public DiscGolfService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<List<DiscGolfEvent>> GetEventsAsync()
+        {
+            return await httpClient.GetFromJsonAsync<List<DiscGolfEvent>>(
+                "http://localhost:24001/api/disc-golf-service/public/events");
+        }
+    }
+}
