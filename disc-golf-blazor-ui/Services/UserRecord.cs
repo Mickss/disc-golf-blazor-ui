44c9709ec549c6249a910c63f4cb6d574fe537c2
Michał Guz <michal.guz.85@gmail.com>
2025-07-07 13:39:46 +0200

    DG-96 Adding an Users display table.

---
+++ disc-golf-blazor-ui/Services/UserRecord.cs
@@ -0,0 +1,9 @@
+﻿namespace disc_golf_blazor_ui.Components.Services
+{
+    public class UserRecord
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
