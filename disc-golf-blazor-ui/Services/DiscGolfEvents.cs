971b42e7ba49b672421f7783c62788bbfb929a14
Michał Guz <michal.guz.85@gmail.com>
2025-07-07 12:35:51 +0200

    DG-95 Adding an Event display table.

---
+++ disc-golf-blazor-ui/Services/DiscGolfEvents.cs
@@ -0,0 +1,12 @@
+﻿namespace disc_golf_blazor_ui.Components.Services
+{
+    public class DiscGolfEvent
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Pdga { get; set; } = string.Empty;
+        public string TournamentTitle { get; set; } = string.Empty;
+        public string Region { get; set; } = string.Empty;
+        public string Registration { get; set; } = string.Empty;
+        public string Vacancies { get; set; } = string.Empty;
+    }
+}
---
+++ disc-golf-blazor-ui/Services/DiscGolfService.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http.Json;
+
+namespace disc_golf_blazor_ui.Components.Services;
+    public class DiscGolfService
+{
+    private readonly HttpClient httpClient;
+
+    public DiscGolfService(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public async Task<List<DiscGolfEvent>> GetEventsAsync()
+    {
+        return await httpClient.GetFromJsonAsync<List<DiscGolfEvent>>(
+            "http://localhost:24001/api/disc-golf-service/public/events");
+    }
+}
